a42cb64ba5c5b8168cd1b1d9f7d00dd0c6857709
pshegde123 <pradnyahegde@gmail.com>
2025-08-21 15:18:16 -0400

    added error check for repeated letters

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ const searchableWords = [
     'vision',
     'deadpool',
     'professorx',
-    'magnato',
+    'magne to',
     'iceman'
 ];
 const maxAttempts = 10;
@@ -24,6 +24,7 @@ const randomIndex = Math.floor(Math.random() * searchableWords.length);
 var guessingWord = [];
 let guessedLetters = [];
 let isGameOver = false;
+let gameWon = false;
 var positions = [];
 var remainingGuesses = 0; // How many tries the player has left
 var currentSelectedWord = '';
@@ -32,7 +33,7 @@ var currentSelectedWord = '';
 /* Display the current word as a series of '-'*/
 const loadGame = () => {
     currentSelectedWord = searchableWords[randomIndex];
-    console.log('Selected word is:', currentSelectedWord);
+    //console.log('Selected word is:', currentSelectedWord);    
     document.getElementById('winImage').style.display = 'none';
     document.getElementById('gameoverImage').style.display = 'none';
     document.getElementById('hangmanImage').style.display = 'none';
@@ -55,34 +56,53 @@ const playGame = (code) =>{
     updateImage();
 }
 const evaluateGuess = (letter) => {
-    // Check if the guessed letter is in the current selected word
+    positions = [];    
+
+    // Check if the guessed letter is in the current selected word 
+    // Do nothing if letter is already in the guessedWord array
     for (let i = 0; i < currentSelectedWord.length; i++) {       
         if (currentSelectedWord[i].toLowerCase() === letter.toLowerCase()) {
             positions.push(i);            
         }
     }
-     //console.log('positions:', positions);
-     // if there are no indicies, remove a guess and update the hangman image
+    console.log('positions:', positions);
+
+    // if there are no position indicies, remove a guess and update the hangman image
     if (positions.length <= 0) {
-        // incorrect guesses
-        guessedLetters.push(letter);
-        document.getElementById("incorrectGuesses").textContent = guessedLetters.join(', ');       
-    } else {
-        // Loop through all the indicies and replace the '_' with a letter.
-        for(var i = 0; i < positions.length; i++) {           
-            guessingWord[positions[i]] = letter;            
+        // incorrect guesses        
+        // Add letter to guessedLetters array
+        // Do nothing if letter is already in the guessedLetters array
+        if(!guessedLetters.includes(letter)){
+            guessedLetters.push(letter);
+            document.getElementById("incorrectGuesses").textContent = guessedLetters.join(', '); 
+            remainingGuesses--;      
         }
-    }    
-    positions = [];
-    remainingGuesses--;
+    } else {
+        // correct guesses        
+        if(guessingWord.includes(letter)){
+            // Do nothing if letter is already in the guessedWord array               
+            return;
+        }
+        else{           
+            //console.log('Replacing positions:', positions);
+            //console.log('Letters guessed : ', guessingWord);
+            
+            // Loop through all the indicies and replace the '_' with a letter.
+            for(var i = 0; i < positions.length; i++) {           
+                guessingWord[positions[i]] = letter;            
+            }
+            remainingGuesses--;
+        }
+    }        
 };
 const updateImage = () =>{    
     document.getElementById('hangmanImage').style.display = 'block';
+     document.getElementById('hangmanImage').classList.remove('bounce'); 
     // Update the hangman image based on the number of remaining guesses    
     document.getElementById('hangmanImage').src = `./assets/images/${maxAttempts - remainingGuesses}.jpg`;
 }
 const checkLoss = ()=> {
-    if(remainingGuesses <=0){
+    if(remainingGuesses <=0 && !gameWon){
         document.getElementById('hangmanImage').classList.add('bounce'); 
         document.getElementById('hangmanImage').src = './assets/images/lose.jpg';
         gameLostSound.play()
@@ -90,11 +110,12 @@ const checkLoss = ()=> {
     }
 }
 const checkWin = ()=> {
-    if(remainingGuesses >0 && guessingWord.join('') === currentSelectedWord){      
+    if(remainingGuesses >=0 && guessingWord.join('') === currentSelectedWord){      
         document.getElementById('hangmanImage').classList.add('bounce'); 
         document.getElementById('hangmanImage').src = './assets/images/you-win.png';        
         gameWonSound.play()
         isGameOver = true;
+        gameWon = true;
     }
 }
 const updateDisplay=(code)=>{
@@ -117,6 +138,7 @@ const resetGame = () => {
         guessingWord.push(" _ ");
     }   
     updateDisplay();
+    window.location.reload();
 }
 
 loadGame();   
@@ -127,9 +149,10 @@ document.addEventListener('keydown',(event)=>{
     }
     else if(event.keyCode >= 65 && event.keyCode <= 90) // A-Z
     {            
-        playGame(event.key);
+        playGame(event.key.toLocaleLowerCase());
+        checkWin();     
         checkLoss();
-        checkWin();
+        
     }
     
 });
